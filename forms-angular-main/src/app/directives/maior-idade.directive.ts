ae9bcbc40acd555396ebf0e86e22c22f5f538b15
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-08-29 21:51:42 -0300

    Diretiva de validação

--- forms-angular-main/src/app/directives/maior-idade.directive.ts
+++ forms-angular-main/src/app/directives/maior-idade.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive } from "@angular/core";
+import {
+  AbstractControl,
+  NG_VALIDATORS,
+  ValidationErrors,
+  Validators,
+} from "@angular/forms";
 
 @Directive({
-  selector: '[appMaiorIdade]'
+  selector: "[appMaiorIdade]",
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: MaiorIdadeDirective,
+      multi: true,
+    },
+  ],
 })
-export class MaiorIdadeDirective {
+export class MaiorIdadeDirective implements Validators {
+  constructor() {}
 
-  constructor() { }
+  validate(control: AbstractControl): ValidationErrors | null {
+    const dataNascimento = control.value;
+    const anoNascimento = new Date(dataNascimento).getFullYear();
+    const anoNascimentoMais18 = anoNascimento + 18;
+    const anoAtual = new Date().getFullYear();
 
+    const ehMaior = anoNascimentoMais18 <= anoAtual;
+
+    return ehMaior ? null : { maiorIdadeValidator: true };
+  }
 }
